84800aaa02095a8dc7210f4e222cd45072afdbf1
gouf <innocent.zero@gmail.com>
2015-02-02 12:59:06 +0900

    Add initial spec files

---
+++ spec/controllers/postages_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PostagesController, :type => :controller do
+
+end
---
+++ spec/factories/prefectures.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :prefecture do
+    name "MyString"
+postage "9.99"
+  end
+
+end
