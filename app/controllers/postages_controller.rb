b8c026def811dd44c87e031e5fef9405615d138f
gouf <innocent.zero@gmail.com>
2015-02-02 12:56:32 +0900

    Add postage calc action for ajax

--- app/controllers/postages_controller.rb
+++ app/controllers/postages_controller.rb
@@ -2,4 +2,18 @@ class PostagesController < ApplicationController
   def index
     @prefectures = Prefecture.all
   end
+
+  def postage
+    name = postage_params[:name]
+    @postage = Prefecture.find_by_name(name)
+    respond_to do |format|
+      format.json { render json: @postage }
+    end
+  end
+
+  private
+
+  def postage_params
+    params.permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :postages
   get  'postages/index'
+  post 'postages/postage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
